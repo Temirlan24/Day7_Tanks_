f4bc0d8a4f45e58704876a1fb4a73e581f5296d4
Temirlan24 <52044447+Temirlan24@users.noreply.github.com>
2019-07-13 13:01:08 +0600

    Создал класс Tank и продемонстрировал сражение в решении.

---
+++ Day7Tanks/Day7Tanks/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using MyClassLib.WorldOfTanks;
+
+namespace Day7_Tanks_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Thread.Sleep(20);
+            const string firstTank = "T-34";
+            const string secondTank = "Pantera";
+            int ammunition, armor, mobility;
+            const int countOfTanks = 5;
+            Tank[] t34 = new Tank[countOfTanks];
+            Tank[] pantera = new Tank[countOfTanks];
+            for (int i = 0; i < countOfTanks; i++)
+            {
+                Console.WriteLine($"{i + 1} схватка: ");
+                ammunition = random.Next(0, 100);
+                armor = random.Next(0, 100);
+                mobility = random.Next(0, 100);
+                t34[i] = new Tank(firstTank, ammunition, armor, mobility);
+                t34[i].ShowTanks();
+                Console.WriteLine();
+                ammunition = random.Next(0, 100);
+                armor = random.Next(0, 100);
+                mobility = random.Next(0, 100);
+                pantera[i] = new Tank(secondTank, ammunition, armor, mobility);
+                pantera[i].ShowTanks();
+                Console.WriteLine();
+            }
+            Tank[] win = new Tank[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                win[i] = new Tank();
+                win[i] = t34[i] * pantera[i];
+
+                Console.WriteLine();
+                Console.WriteLine($"В {i + 1} схватке победил");
+                win[i].ShowTanks();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day7Tanks/Day7Tanks/Tank.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib.WorldOfTanks
+{
+    public class Tank
+    {
+        public string TheNameOfTank { get; set; }// название танка
+        public int LevelTankAmmunition { get; set; } //уровень боекомплекта танка
+        public int TankArmor { get; set; } // уровень брони
+        public int MobilityOfTank { get; set; } // уровень маневренности
+
+        public Tank()
+        {
+
+        }
+        public Tank(string TheNameOfTank, int LevelTankAmmunition, int TankArmor, int MobilityOfTank)
+        {
+            this.TheNameOfTank = TheNameOfTank;
+            this.LevelTankAmmunition = LevelTankAmmunition;
+            this.TankArmor = TankArmor;
+            this.MobilityOfTank = MobilityOfTank;
+        }
+        public void ShowTanks()
+        {
+            Console.WriteLine("Название танка:\t\t\t" + TheNameOfTank);
+            Console.WriteLine("Уровень боекомплекта танка:\t" + LevelTankAmmunition);
+            Console.WriteLine("Уровень брони:\t\t\t" + TankArmor);
+            Console.WriteLine("Уровень маневренности:\t\t" + MobilityOfTank);
+        }
+        public static Tank operator *(Tank FirstTank, Tank SecondTank)
+        {
+
+            if (FirstTank.LevelTankAmmunition > SecondTank.LevelTankAmmunition && FirstTank.TankArmor > SecondTank.TankArmor
+                || FirstTank.LevelTankAmmunition > SecondTank.LevelTankAmmunition && FirstTank.MobilityOfTank > SecondTank.MobilityOfTank
+                || FirstTank.TankArmor > SecondTank.TankArmor && FirstTank.MobilityOfTank > SecondTank.MobilityOfTank
+                || FirstTank.LevelTankAmmunition > SecondTank.LevelTankAmmunition && FirstTank.TankArmor > SecondTank.TankArmor && FirstTank.MobilityOfTank > SecondTank.MobilityOfTank)
+            {
+                return FirstTank;
+            }
+            else if (FirstTank.LevelTankAmmunition < SecondTank.LevelTankAmmunition && FirstTank.TankArmor < SecondTank.TankArmor
+                || FirstTank.LevelTankAmmunition < SecondTank.LevelTankAmmunition && FirstTank.MobilityOfTank < SecondTank.MobilityOfTank
+                || FirstTank.TankArmor < SecondTank.TankArmor && FirstTank.MobilityOfTank < SecondTank.MobilityOfTank
+                || FirstTank.LevelTankAmmunition < SecondTank.LevelTankAmmunition && FirstTank.TankArmor < SecondTank.TankArmor && FirstTank.MobilityOfTank > SecondTank.MobilityOfTank)
+            {
+                return SecondTank;
+            }
+            return SecondTank;
+        }
+    }
+}
